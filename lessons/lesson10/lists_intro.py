0e6d6f5fa192a664cfcd9c6155ecdeb5af2ed399
desaiViraj <desaiviraj@yahoo.com>
2026-07-03 16:42:05 +0530

    lets list.

---
+++ lessons/lesson10/lists_intro.py
@@ -0,0 +1,9 @@
+servers = ["web01", "web02", "db01", "db02"]
+
+print(type(servers))
+print(len(servers))
+print(servers[0])
+print(servers[1])
+print(servers[2])
+print(servers[-1])
+print(servers)
